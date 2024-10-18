a5c249644b3c9ee246d55b81de8e842148674c0a
Estebern <estebern4042@gmail.com>
2024-10-17 21:10:02 -0700

    Initial commit for yii2-alert extension

---
+++ alert.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace hscstudio\alert;
+
+use Yii;
+
+class Alert extends \yii\bootstrap5\Widget
+{
+    public $alertTypes = [
+        'error'   => 'alert-danger',
+        'danger'  => 'alert-danger',
+        'success' => 'alert-success',
+        'info'    => 'alert-info',
+        'warning' => 'alert-warning'
+    ];
+
+    public $closeButton = [];
+
+    public function run()
+    {
+        $session = Yii::$app->session;
+        $appendClass = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
+
+        foreach (array_keys($this->alertTypes) as $type) {
+            $flash = $session->getFlash($type);
+
+            foreach ((array) $flash as $i => $message) {
+                echo \yii\bootstrap5\Alert::widget([
+                    'body' => $message,
+                    'closeButton' => $this->closeButton,
+                    'options' => array_merge($this->options, [
+                        'id' => $this->getId() . '-' . $type . '-' . $i,
+                        'class' => $this->alertTypes[$type] . $appendClass,
+                    ]),
+                ]);
+            }
+
+            $session->removeFlash($type);
+        }
+    }
+}
